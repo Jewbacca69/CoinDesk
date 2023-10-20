c206b723eb9b8e4a87b58ac1756acd8dba2a0d51
Jewbacca69 <141155995+Jewbacca69@users.noreply.github.com>
2023-10-20 17:57:58 +0300

    Update main.php

--- main.php
+++ main.php
@@ -3,13 +3,9 @@
 require_once "vendor/autoload.php";
 
 use Symfony\Component\Console\Application;
-use CoinDesk\CoinDesk;
-use CoinDesk\CoinDeskAPI;
 
-$coinDesk = new CoinDesk(new CoinDeskAPI());
 $application = new Application();
-
-$application->add(new \CoinDesk\Application($coinDesk));
+$application->add(new \CoinDesk\Application());
 
 try {
     $application->run();
